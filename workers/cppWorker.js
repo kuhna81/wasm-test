87f1a237407179a3752368add8816138164a6b3f
Andrew Kuhn <kuhna81@gmail.com>
2026-09-11 13:18:40 +0000

    pre-intialize wasm module before call

--- workers/cppWorker.js
+++ workers/cppWorker.js
@@ -1,33 +1,39 @@
-let factory, moduleInstance;
+let factory, moduleInstance, wasmReady;
+
+const modulePath = '../cpp/lib.js';
+const pathPrefix = '../cpp/';
 
 async function init() {
   if (moduleInstance) return moduleInstance;
-  factory = (await import('../cpp/lib.js')).default;
+  factory = (await import(modulePath)).default;
   moduleInstance = await factory({
-    locateFile: (filename) => '../cpp/' + filename
+    locateFile: (filename) => pathPrefix + filename
   });
   return moduleInstance;
 }
 
+(async () => {
+	factory = (await import(modulePath)).default;
+
+	wasmReady = factory({
+		locateFile: (filename) => pathPrefix + filename
+	}).then((instance) => {
+		moduleInstance = instance;
+		console.log('wasm module initialized');
+	});
+})();
+
 self.onmessage = async ({ data }) => {
-	/*
-	const mod = await init();
-	const r = mod.sieve_primes(20);
-	console.log('repr:', r.toString?.(), r);
-	console.log('has size():', typeof r.size === 'function');
-	console.log('size():', typeof r.size === 'function' ? r.size() : 'no size');
-	console.log('first few:', typeof r.get === 'function' ? [r.get(0), r.get(1), r.get(2)] : 'no get');
-	*/
-  const { limit } = data;
-  const t0 = performance.now();
-  const mod = await init();
+	const { limit } = data;
 
-  const primeNumbers = mod.sieve_primes_js(limit);
+	const t0 = performance.now();
+	//const mod = await init();
+	const primeNumbers = moduleInstance.sieve_primes_js(limit);
 
-  const t1 = performance.now();
-  self.postMessage({
-    engineName: 'WASM (C++ via Emscripten)',
-    elapsedTimeMs: t1 - t0,
-    primeNumbers
-  });
+	const t1 = performance.now();
+	self.postMessage({
+		engineName: 'WASM (C++ via Emscripten)',
+		elapsedTimeMs: t1 - t0,
+		primeNumbers
+	});
 };
